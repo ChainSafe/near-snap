3b307313c480e9510467924f2e27bed47f96280f
irubido <ivan.rubido@gmail.com>
2022-07-28 14:42:11 +0200

    comment remove

--- packages/snap/src/interfaces.ts
+++ packages/snap/src/interfaces.ts
@@ -1,6 +1,3 @@
 import { JsonRpcRequest } from '@metamask/types';
 
-// export type SnapRpcRequest = { options: { method: string; params?: unknown[] } }
 export type SnapRpcRequest = JsonRpcRequest<unknown[] | { [key: string]: unknown }>
-// export type SnapRpcRequest = any;
-
