2b884a9b880b437452ca5a38e574f8d53a8c89fb
Bernard <bero4net@gmail.com>
2022-08-09 12:44:16 +0200

    chore: improve validation types

--- packages/snap/src/utils/params.ts
+++ packages/snap/src/utils/params.ts
@@ -1,12 +1,25 @@
-import { object, string, array } from "superstruct";
+import { object, any, array, defaulted, Describe, enums } from "superstruct";
+import { transactions } from "near-api-js";
+import { NearNetwork, SignTransactionsParams } from "../interfaces";
 
-const networkSchema = string();
+export const networkSchemaDefaulted: Describe<NearNetwork> = defaulted(
+  enums(["testnet", "mainnet"]),
+  () => "testnet"
+);
 
-export const signTransactionsSchema = object({
+export const networkSchema: Describe<NearNetwork> = enums([
+  "testnet",
+  "mainnet",
+]);
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+const transaction: Describe<transactions.Transaction> = any();
+
+export const signTransactionsSchema: Describe<SignTransactionsParams> = object({
   network: networkSchema,
-  transactions: array(),
+  transactions: array(transaction),
 });
 
-export const validAccountSchema = object({
+export const validAccountSchema: Describe<{ network: NearNetwork }> = object({
   network: networkSchema,
 });
