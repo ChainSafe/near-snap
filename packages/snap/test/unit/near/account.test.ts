2ea5aee51f12027645e808f55f15dca5670902af
Bernard <bero4net@gmail.com>
2022-09-08 16:38:30 +0200

    fix: tests

--- packages/snap/test/unit/near/account.test.ts
+++ packages/snap/test/unit/near/account.test.ts
@@ -3,7 +3,7 @@ import sinonChai from "sinon-chai";
 import sinon from "sinon";
 import { mockSnapProvider } from "../wallet.stub";
 import { getKeyPair } from "../../../src/near/account";
-import { bip44Entropy397Node } from "./bip44Entropy.mock";
+import { bip32Entropy397Node } from "./bip32Entropy.mock";
 
 chai.use(sinonChai);
 
@@ -17,12 +17,23 @@ describe("Test account function: getKeyPair", function () {
 
   it("should return valid keypair for near mainnet", async function () {
     walletStub.request
-      .withArgs(sinon.match.has("method", "snap_getBip44Entropy_397"))
-      .resolves(bip44Entropy397Node);
+      .withArgs(sinon.match.has("method", "snap_getBip32Entropy"))
+      .resolves(bip32Entropy397Node);
 
     const keypair = await getKeyPair(walletStub, "mainnet");
     expect(keypair.toString()).to.be.eq(
-      "ed25519:2wiQ5A1yzo3jq235NAeHphkcir8DJKsgSYR1mDygP3rSsCd2hGhXF71UjKVEzE3xCg3e8HtKSiEaypySwLnMM61R"
+      "ed25519:2eCVNyWDRbYxD54BxAKeJrom7dJYJD1GiBsSq3TZKFUGuyvCcEDv7Fm9aHp6QNCeyUHURsvxSwhrqTiQ2gYKjXHZ"
+    );
+  });
+
+  it("should return valid keypair for near testnet", async function () {
+    walletStub.request
+      .withArgs(sinon.match.has("method", "snap_getBip32Entropy"))
+      .resolves(bip32Entropy397Node);
+
+    const keypair = await getKeyPair(walletStub, "testnet");
+    expect(keypair.toString()).to.be.eq(
+      "ed25519:2eCVNyWDRbYxD54BxAKeJrom7dJYJD1GiBsSq3TZKFUGuyvCcEDv7Fm9aHp6QNCeyUHURsvxSwhrqTiQ2gYKjXHZ"
     );
   });
 });
