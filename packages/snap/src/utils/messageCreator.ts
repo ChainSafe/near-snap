c594f7dc786b92ab7bbadab3b2236896ecb087a1
irubido <ivan.rubido@gmail.com>
2022-09-26 14:44:41 +0200

    display signing params

--- packages/snap/src/utils/messageCreator.ts
+++ packages/snap/src/utils/messageCreator.ts
@@ -1,9 +1,12 @@
+import { AddKeyAction, CreateAccountAction, DeleteAccountAction, DeleteKeyAction, DeployContractAction, FunctionCallAction, StakeAction, TransferAction } from "@near-wallet-selector/core";
 import { TransactionJson } from "../interfaces";
 
 export interface Message {
   message: string;
   value: unknown | undefined;
 }
+
+type ActionWithParams = DeployContractAction | FunctionCallAction | TransferAction | StakeAction | DeleteKeyAction | DeleteAccountAction
 
 export const messageCreator = (txArray: TransactionJson[]): string => {
   const messages: Message[] = [];
@@ -12,9 +15,30 @@ export const messageCreator = (txArray: TransactionJson[]): string => {
     messages.push(msgTo);
     for (const txDataAction of txData.actions) {
       messages.push({
-        message: "transaction data action",
+        message: "Transaction data action:",
         value: txDataAction.type,
       });
+      if ((txDataAction as unknown as ActionWithParams).params) {
+        const { params } = (txDataAction as unknown as ActionWithParams);
+        for (const [key, value] of Object.entries(params)) {
+          if (typeof value === "object") {
+            messages.push({
+              message: `${key}: `,
+              value: "",
+            });
+            for (const [keyDeep, valueDeep] of Object.entries(value)) {
+              messages.push({
+                message: `  ${keyDeep}: `,
+                value: valueDeep,
+              });
+            }
+          } else
+            messages.push({
+              message: `${key}: `,
+              value,
+            });
+        }
+      }
     }
   }
   return messages
